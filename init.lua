5de1c0b5057e14087457ea054d85cb157379cc6d
aslink87 <adam.slinkman@gmail.com>
2022-08-19 11:13:35 -0400

    feat: updated to ecovim v2.1.0

--- init.lua
+++ init.lua
@@ -1,110 +1,17 @@
--- Install Packer
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+pcall(require, 'impatient')
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
---local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
---vim.api.nvim_create_autocmd('BufWritePost', { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'init.lua' })
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- colors/ui
-  use 'ishan9299/nvim-solarized-lua'
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use 'preservim/nerdcommenter'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'glepnir/dashboard-nvim'
-  --use 'lukas-reineke/indent-blankline.nvim'
-  -- telescope
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  -- treesitter
-  use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-  }
-  use 'nvim-treesitter/playground'
-  -- QOL
-  use 'windwp/nvim-ts-autotag'
-  use 'ludovicchabant/vim-gutentags'
-  use 'kevinoid/vim-jsonc'
-  use 'windwp/nvim-autopairs'
-  use 'scrooloose/NERDTree'
-  -- lsp
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
-  use 'onsails/lspkind-nvim'
-  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
-  -- cmp
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use "f3fora/cmp-spell"
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/cmp-vsnip'
-  -- null-ls
-  --use 'jose-elias-alvarez/null-ls.nvim'
-  -- snippets
-  use 'rafamadriz/friendly-snippets'
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
-
--- imports
+require('internal.packer_commands')
+require('utils.globals')
+require('config')
 require('settings')
-require('keymaps')
-require('themes')
-require('commands')
-require('lsp')
---require('nullls')
-require('autopairs')
-require('airline')
-require('comp')
-require('gutentags')
-require('treesitter')
-require('icons')
---require('telescope')
+require('colorscheme')
+require('keymappings')
+require('autocmds')
+require('functions')
+require('internal.cursorword')
 
---telescope
-require('telescope').setup{
-  defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    file_ignore_patterns = { "^node_modules/" },
-    mappings = {
-      i = {
-        -- map actions.which_key to <C-h> (default: <C-/>)
-        -- actions.which_key shows the mappings for your picker,
-        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key"
-      }
-    }
-  },
-  pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    }
-  }
-}
+require('lsp.config')
+require('lsp.setup')
+require('lsp.functions')
 
-require('telescope').load_extension('fzf')
+require('snippets.react')
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,75 +1,72 @@
-local o = vim.opt
+local options = {
+  clipboard      = "unnamed,unnamedplus",   --- Copy-paste between vim and everything else
+  cmdheight      = 2,                       --- Give more space for displaying messages
+  completeopt    = "menu,menuone,noselect", --- Better autocompletion
+  cursorline     = true,                    --- Highlight of current line
+  emoji          = false,                   --- Fix emoji display
+  expandtab      = true,                    --- Use spaces instead of tabs
+  foldcolumn     = "0",
+  foldnestmax    = 0,
+  foldlevel      = 99,                      --- Using ufo provider need a large value
+  foldlevelstart = 99,                      --- Expand all folds by default
+  ignorecase     = true,                    --- Needed for smartcase
+  laststatus     = 3,                       --- Have a global statusline at the bottom instead of one for each window
+  lazyredraw     = true,                    --- Makes macros faster & prevent errors in complicated mappings
+  mouse          = "a",                     --- Enable mouse
+  number         = true,                    --- Shows current line number
+  pumheight      = 10,                      --- Max num of items in completion menu
+  relativenumber = true,                    --- Enables relative number
+  scrolloff      = 8,                       --- Always keep space when scrolling to bottom/top edge
+  shiftwidth     = 2,                       --- Change a number of space characeters inseted for indentation
+  showtabline    = 2,                       --- Always show tabs
+  signcolumn     = "yes:2",                 --- Add extra sign column next to line number
+  smartcase      = true,                    --- Uses case in search
+  smartindent    = true,                    --- Makes indenting smart
+  smarttab       = true,                    --- Makes tabbing smarter will realize you have 2 vs 4
+  softtabstop    = 2,                       --- Insert 2 spaces for a tab
+  splitright     = true,                    --- Vertical splits will automatically be to the right
+  swapfile       = false,                   --- Swap not needed
+  tabstop        = 2,                       --- Insert 2 spaces for a tab
+  termguicolors  = true,                    --- Correct terminal colors
+  timeoutlen     = 200,                     --- Faster completion (cannot be lower than 200 because then commenting doesn't work)
+  undofile       = true,                    --- Sets undo to file
+  updatetime     = 100,                     --- Faster completion
+  viminfo        = "'1000",                 --- Increase the size of file history
+  wildignore     = "*node_modules/**",      --- Don't search inside Node.js modules (works for gutentag)
+  wrap           = false,                   --- Display long lines as just one line
+  writebackup    = false,                   --- Not needed
+  -- Neovim defaults
+  autoindent     = true,                    --- Good auto indent
+  backspace      = "indent,eol,start",      --- Making sure backspace works
+  backup         = false,                   --- Recommended by coc
+  conceallevel   = 0,                       --- Show `` in markdown files
+  encoding       = "utf-8",                 --- The encoding displayed
+  errorbells     = false,                   --- Disables sound effect for errors
+  fileencoding   = "utf-8",                 --- The encoding written to file
+  incsearch      = true,                    --- Start searching before pressing enter
+  showmode       = false,                   --- Don't show things like -- INSERT -- anymore
+}
 
---Make line numbers default
-vim.wo.number = true
+local globals = {
+  mapleader                   = ' ',        --- Map leader key to SPC
+  speeddating_no_mappings     = 1,          --- Disable default mappings for speeddating
+}
 
---Enable mouse mode
-vim.o.mouse = 'a'
+vim.opt.shortmess:append('c');
+vim.opt.formatoptions:remove('c');
+vim.opt.formatoptions:remove('r');
+vim.opt.formatoptions:remove('o');
+vim.opt.fillchars:append('stl: ');
+vim.opt.fillchars:append('eob: ');
+vim.opt.fillchars:append('fold: ');
+vim.opt.fillchars:append('foldopen: ');
+vim.opt.fillchars:append('foldsep: ');
+vim.opt.fillchars:append('foldclose:');
 
---Save undo history
-vim.opt.undofile = true
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
---Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
---Decrease update time
-vim.o.updatetime = 250
-
---Set colorscheme (order is important here)
-vim.o.termguicolors = true
---vim.cmd [[colorscheme gruvbox]]
-
--- global settings
-o.hidden = true
-o.shiftwidth = 2
-o.tabstop = 2
-o.expandtab = true
-o.smarttab = true
-o.showtabline = 2
-o.autoindent = true
-o.smartindent = true
-o.undofile = true
-o.cursorline = false
-o.relativenumber = true
-o.wrap = false
-o.list = true
-o.signcolumn = 'yes'
-o.undodir = vim.fn.stdpath('config') .. '/undo'
-o.laststatus = 0
-o.showmode = false
-o.ignorecase = true
-o.scrolloff = 8
-o.sidescrolloff = 8
-o.shiftround = true
-o.smartcase = true
-o.splitbelow = true
-o.splitright = true
-o.inccommand = 'nosplit'
-o.errorbells = false
-o.swapfile = false
-o.backup = false
-o.cmdheight = 1
-o.pumheight = 25
-o.pumblend = 10
-o.syntax = 'enable'
-o.colorcolumn = '80'
-o.lazyredraw = true
-o.foldlevel = 99
-o.foldmethod = 'expr'
-o.foldexpr = 'nvim_treesitter#foldexpr()'
-o.formatoptions = 'cro'
-o.clipboard = 'unnamedplus'
-o.wildignore = '*/node_modules/*'
-
-vim.cmd([[
-    filetype indent plugin on
-    au BufNewFile,BufRead *.es6 setf javascript
-    au BufNewFile,BufRead *.tsx setf typescriptreact
-    au BufNewFile,BufRead *.jsx setf javascriptreact
-    au BufNewFile,BufRead *.md setf markdown
-    au BufNewFile,BufRead *.mdx setf markdown
-    set suffixesadd=.js,.es,.jsx,.json,.css,.less,.sass,.styl,.php,.py,.md
-]])
-
-_G.global = {}
+for k, v in pairs(globals) do
+  vim.g[k] = v
+end
