6a3eaf4e5c43e5318f94def1406f92216e618a0e
aslink87 <adam.slinkman@gmail.com>
2023-01-11 11:37:06 -0500

    initial settings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap.set
+local silent = { silent = true }
+
+-- Move highlight text around
+keymap("x", "K", ":move '<-2<CR>gv-gv", silent)
+keymap("x", "J", ":move '>+1<CR>gv-gv", silent)
+
+-- Dont yank on visual paste
+keymap("v", "p", '"_dP', silent)
+
+-- Dont yank on delete char
+keymap("n", "x", '"_x', silent)
+keymap("n", "X", '"_X', silent)
+keymap("v", "x", '"_x', silent)
+keymap("v", "X", '"_X', silent)
