71909773535cee5682704f71e007f2e2b8af9322
Adam Slinkman <adam.slinkman@gmail.com>
2023-02-13 14:37:51 -0500

    feat(plugins): added neo-tree, config file allowing tokyonight transparency

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,12 @@
+local M = {
+    "folke/tokyonight.nvim",
+    opts = {
+        transparent = false,
+        styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+        },
+    },
+}
+
+return M
