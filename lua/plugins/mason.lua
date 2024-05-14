6ba2b2fe2d6aae29d59b0cc7b8ece79a3aa20ee4
Adam Slinkman <adam.slinkman@gmail.com>
2024-05-14 12:34:35 -0400

    pkg update 14 May 2024, mason config update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,58 +1,35 @@
 return {
   "williamboman/mason.nvim",
-  dependencies = {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  cmd = "Mason",
+  keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+  build = ":MasonUpdate",
+  opts = {
+    ensure_installed = {
+      "bash-language-server",
+      "black",
+      "clang-format",
+      "css-lsp",
+      "dockerfile-language-server",
+      "eslint_d",
+      "eslint-lsp",
+      "html-lsp",
+      "json-lsp",
+      "lua-language-server",
+      "markdownlint",
+      "php-cs-fixer",
+      "prettierd",
+      "python-lsp-server",
+      "rust-analyzer",
+      "shellcheck",
+      "shellharden",
+      "shfmt",
+      "stylua",
+      "svelte-language-server",
+      "tailwindcss-language-server",
+      "terraform-ls",
+      "tflint",
+      "typescript-language-server",
+      "yaml-language-server",
+    },
   },
-  build = ":MasonInstallAll",
-  config = function()
-    local f = require("utils.functions")
-    local mason_tool_installer = require("mason-tool-installer")
-    require("mason").setup({
-      ui = {
-        border = "shadow",
-        icons = require("utils.icons").mason,
-        zindex = 99,
-      },
-    })
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "prettierd", -- prettierd formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint", -- python linter
-        "eslint_d", -- js linter
-        "markdownlint", -- markdown linter
-      },
-    })
-    f.cmd("MasonInstallAll", function()
-      vim.cmd("MasonUpdate")
-      local ensure_installed = {
-        "bash-language-server",
-        "black",
-        "clang-format",
-        "css-lsp",
-        "dockerfile-language-server",
-        "eslint-lsp",
-        "html-lsp",
-        "json-lsp",
-        "lua-language-server",
-        "php-cs-fixer",
-        "prettierd",
-        "python-lsp-server",
-        "rust-analyzer",
-        "shellcheck",
-        "shellharden",
-        "shfmt",
-        "stylua",
-        "tailwindcss-language-server",
-        "terraform-ls",
-        "tflint",
-        "typescript-language-server",
-        "yaml-language-server",
-      }
-      vim.cmd("MasonInstall " .. table.concat(ensure_installed, " "))
-    end, { desc = "install all lsp tools" })
-  end,
 }
