9377ba1921792fa83a811b195fc72e4bef7d5374
aslink87 <adam.slinkman@gmail.com>
2024-05-17 12:02:48 -0400

    refactor treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,65 +1,73 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
-
-    ---@diagnostic disable-next-line
-    treesitter.setup({
-      ensure_installed = {
-        "bash",
-        "css",
-        "dockerfile",
-        "go",
-        "graphql",
-        "hcl",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "php",
-        "python",
-        "regex",
-        "regex",
-        "ruby",
-        "rust",
-        "scss",
-        "sql",
-        "svelte",
-        "terraform",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-      highlight = {
-        enable = true,
-      },
-      match = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "zi",
-          node_incremental = "zi",
-          scope_incremental = "zo",
-          node_decremental = "zd",
-        },
-      },
-      indent = {
-        enable = true,
-      },
-      pickers = {
-        find_files = {
-          hidden = true,
-        },
-      },
-    })
+  -- event = { "BufReadPost", "BufNewFile" },
+  event = { "LazyFile", "VeryLazy" },
+  lazy = vim.fn.argc(-1) == 0,
+  init = function(plugin)
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require("nvim-treesitter.query_predicates")
   end,
-  build = function()
-    vim.cmd([[TSUpdate]])
+  keys = {
+    { "<c-space>", desc = "Increment Selection" },
+    { "<bs>", desc = "Decrement Selection", mode = "x" },
+  },
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  opts = {
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+      },
+    },
+    ensure_installed = {
+      "bash",
+      "css",
+      "dockerfile",
+      "go",
+      "graphql",
+      "hcl",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "php",
+      "python",
+      "regex",
+      "regex",
+      "ruby",
+      "rust",
+      "scss",
+      "sql",
+      "svelte",
+      "terraform",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+  },
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      ---@type table<string, boolean>
+      local added = {}
+      opts.ensure_installed = vim.tbl_filter(function(lang)
+        if added[lang] then
+          return false
+        end
+        added[lang] = true
+        return true
+      end, opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
+    vim.schedule(function()
+      require("lazy").load({ plugins = { "nvim-treesitter-textobjects" } })
+    end)
   end,
 }
