07c101dfbc10a31921dcc2ff8fd3ec42a4dbb64e
aslink87 <adam.slinkman@gmail.com>
2024-06-19 16:25:08 -0400

    inlay hints default to off

---
+++ lua/plugins/lsp/init.lua
@@ -0,0 +1,13 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    -- "folke/neodev.nvim",
+    "b0o/schemastore.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  },
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    inlay_hints = { enabled = false },
+  },
+}
