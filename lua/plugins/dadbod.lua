c5e63b8c7745dc6c779c1512533829f3ca326164
Adam Slinkman <adam.slinkman@gmail.com>
2024-06-27 18:58:15 -0400

    update dadbod, commit db connections

---
+++ lua/plugins/dadbod.lua
@@ -0,0 +1,25 @@
+return {
+  "tpope/vim-dadbod",
+  dependencies = {
+    "kristijanhusak/vim-dadbod-ui",
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    "pbogut/vim-dadbod-ssh",
+  },
+  init = function()
+    local data_path = vim.fn.stdpath("config") .. require("plenary.path").path.sep
+
+    vim.g.db_ui_save_location = data_path .. "db_ui"
+    vim.g.db_ui_auto_execute_table_helpers = 1
+    vim.g.db_ui_show_database_icon = true
+    vim.g.db_ui_save_location = data_path .. "db_ui/tmp"
+    vim.g.db_ui_use_nerd_fonts = true
+    vim.g.db_ui_use_nvim_notify = true
+    vim.g.db_ui_execute_on_save = false
+  end,
+  keys = {
+    { "<leader>Dt", "<cmd>DBUIToggle<cr>", desc = "Toggle UI" },
+    { "<leader>Df", "<cmd>DBUIFindBuffer<cr>", desc = "Find Buffer" },
+    { "<leader>Dr", "<cmd>DBUIRenameBuffer<cr>", desc = "Rename Buffer" },
+    { "<leader>Dq", "<cmd>DBUILastQueryInfo<cr>", desc = "Last Query Info" },
+  },
+}
