368e7781b5ea2924cd7586bbcda5f57b17df151b
aslink87 <adam.slinkman@gmail.com>
2024-06-28 13:40:56 -0400

    update DadBod config

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -11,7 +11,7 @@ return {
     vim.g.db_ui_save_location = data_path .. "db_ui"
     vim.g.db_ui_auto_execute_table_helpers = 1
     vim.g.db_ui_show_database_icon = true
-    vim.g.db_ui_save_location = data_path .. "db_ui/tmp"
+    vim.g.db_ui_tmp_query_location = data_path .. "db_ui/tmp"
     vim.g.db_ui_use_nerd_fonts = true
     vim.g.db_ui_use_nvim_notify = true
     vim.g.db_ui_execute_on_save = false
